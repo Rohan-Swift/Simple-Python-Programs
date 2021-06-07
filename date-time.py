111e6a2fdb7c25fd543a87b87c7dbee329991f9a
Rohan R <sonurohan27@gmail.com>
2021-06-07 22:26:36 +0530

    Print current date and time

---
+++ date-time.py
@@ -0,0 +1,15 @@
+from datetime import date
+from datetime import datetime
+
+today = date.today()
+now = datetime.now()
+
+# DD/MM/YYYY format
+d1 = today.strftime("%d/%m/%Y")
+t1 = now.strftime("%H:%M:%S")
+
+print("Date: ", d1)
+print("Time: ", t1)
+
+
+
